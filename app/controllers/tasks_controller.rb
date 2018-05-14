4f79f8a9857336c7c2d7ec5417ffef6e960375a6
kouhei.kinoshita <kouhei515@icloud.com>
2018-05-14 12:38:44 +0000

    post current_user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
   
   def index
     @tasks = Task.all.page(params[:page]).per(6)
@@ -15,7 +16,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "課題が正常に投稿されました"
